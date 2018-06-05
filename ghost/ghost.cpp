165e09bb3633afaff8e74352be043aadad51516e
Henning Berge <henning@henning.tech>
2018-06-05 19:35:55 +0200

    added test for gamelist

--- ghost/ghost.cpp
+++ ghost/ghost.cpp
@@ -1580,7 +1580,6 @@ bool CGHost :: Update( long usecBlock )
     {
         bool success = m_CallableBotStatusUpdate->GetResult( );
 
-
         m_DB->RecoverCallable( m_CallableBotStatusUpdate );
         delete m_CallableBotStatusUpdate;
         m_CallableBotStatusUpdate = NULL;
